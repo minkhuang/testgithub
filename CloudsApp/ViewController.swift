203a01d076de9b22cb802f6ebc8a7019070fdad0
john2 <john2@yvtcdeiMac-7.local>
2017-07-24 14:38:25 +0800

    試做印出result

--- CloudsApp/ViewController.swift
+++ CloudsApp/ViewController.swift
@@ -14,14 +14,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        Alamofire.request("https://httpbin.org/get").responseJSON{
-            response in
-            print(response.request as Any)
-            print(response.response as Any)
-            print(response.data as Any)
-            print(response.result)
+        Alamofire.request("https://httpbin.org/get").responseJSON{response in
+//            print(response.request as Any)
+//            print(response.response as Any)
+//            print(response.data as Any)
+//            print(response.result)
+            
+//            if let ARRAY = response.result.value{
+//                print("準備印出 result 中的資料")
+//                print(ARRAY)
+//             }
+            
+            if let JSON = response.result.value{
+                if let dictionary = JSON as?[String:Any]{
+                    if let value = dictionary["origin"] as? String{
+                     print("輸出 key 為 origin 的直")
+                     print(value)
+                    }
+                }
+            }
         }
-    }
+   }
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
